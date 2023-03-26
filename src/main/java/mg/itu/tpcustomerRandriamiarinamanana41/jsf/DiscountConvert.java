1b68e6a882794c41cd489c2791a60ac1afc1998b
Onintso <60614002+Onintso@users.noreply.github.com>
2023-03-26 20:56:47 +0200

    creation discountManager.java et modification page details pour gerer le pb avec discount

---
+++ src/main/java/mg/itu/tpcustomerRandriamiarinamanana41/jsf/DiscountConvert.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpcustomerRandriamiarinamanana41.jsf;
+
+import jakarta.ejb.EJB;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.FacesConverter;
+import mg.itu.tpcustomerRandriamiarinamanana41.ejb.DiscountManager;
+import mg.itu.tpcustomerRandriamiarinamanana41.entities.Discount;
+
+/**
+ *
+ * @author randr
+ */
+@FacesConverter(value = "discountConverter", managed=true)
+public class DiscountConvert  implements Converter<Discount>{
+     @EJB
+  private DiscountManager discountManager;
+
+  /**
+   * Convertit une String en Discount.
+   *
+   * @param value valeur à convertir
+   */
+  @Override
+  public Discount getAsObject(FacesContext context, UIComponent component, String value) {
+    if (value == null) return null;
+    return discountManager.findById(value);
+  }
+
+  /**
+   * Convertit un Discount en String.
+   *
+   */
+  @Override
+  public String getAsString(FacesContext context, UIComponent component, Discount discount) {
+    if (discount == null) return "";
+    return discount.getCode();
+  }
+}
